e1af189289efcadf75558dd36bf17f33416a13f4
Frithjof Siebecke <frithjofsiebecke@web.de>
2014-02-27 16:08:05 +0100

    added initialization for TIM12 (buzzer)

--- Driver/led_btn_buz/led_btn_buz.c
+++ Driver/led_btn_buz/led_btn_buz.c
@@ -1,5 +1,9 @@
 #include "led_btn_buz.h"
 #include "stm32f4_discovery.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_tim.h"
+
 
 unsigned ledTimerPeriods[LED_NUM_TIMER] = {
 		LED_TIMER_PERIOD_FLASH,
@@ -7,13 +11,110 @@ unsigned ledTimerPeriods[LED_NUM_TIMER] = {
 		LED_TIMER_PERIOD_SLOW,
 };
 
-enum LED_STATE led_state[LED_NUM_LEDS];
+/* the buzzer acts similar to a led, so there are one more states than leds */
+enum LED_MODE led_mode[LED_NUM_LEDS+1];
+uint8_t led_state[LED_NUM_LEDS+1]; // is the led on or off
+uint8_t led_pulses[LED_NUM_LEDS+1]; // how many pulses to show
 
-/* the buzzer acts similar to a led */
-enum LED_STATE buzzer_state;
+void LedTimerInit ( )
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+
+	// Clock enable
+	RCC_APB1PeriphClockCmd ( RCC_APB1Periph_TIM12, ENABLE );
+
+	// Timer disable
+	TIM_Cmd ( TIM12, DISABLE );
+
+	// Timer init
+	TIM_TimeBaseStructure.TIM_Period =  256 - 1;
+	TIM_TimeBaseStructure.TIM_Prescaler = 328 - 1; // 328 * 256 * 2 ~ 168.000 -> pwm = about 1KHz
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM12, &TIM_TimeBaseStructure);
+
+	// Timer preload enable
+	TIM_ARRPreloadConfig(TIM12, ENABLE);
+
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 127;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC1Init(TIM12, &TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(TIM12, TIM_OCPreload_Enable);
+};
+
+void LedGPIOInit ( )
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+	// Config des Pins als Digital-Ausgang
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	// Alternative-Funktion mit dem IO-Pin verbinden
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_TIM12);
+
+};
+
+void LedOn ( uint8_t ledNum )
+{
+	if ( ledNum < LED_NUM_LEDS )
+	{
+		STM_EVAL_LEDOn ( ledNum );
+	}
+	else if ( ledNum == LED_NUM_LEDS ) // that's the buzzer
+	{
+		TIM_Cmd ( TIM12, ENABLE );
+	};
+};
+
+void LedOff ( uint8_t ledNum )
+{
+	if ( ledNum < LED_NUM_LEDS )
+	{
+		STM_EVAL_LEDOff ( ledNum );
+	}
+	else if ( ledNum == LED_NUM_LEDS ) // that's the buzzer
+	{
+		TIM_Cmd ( TIM12, DISABLE );
+	};
+};
+
+void LedToggle ( uint8_t ledNum )
+{
+	if ( ledNum < LED_NUM_LEDS + 1 )
+	{
+		if ( led_state[ledNum] == 0 )
+		{
+			if ( ledNum == LED_NUM_LEDS ) // that's the buzzer
+				TIM_Cmd ( TIM12, ENABLE );
+			else
+				STM_EVAL_LEDOn ( ledNum );
+			led_state[ledNum] = 1;
+		}
+		else
+		{
+			if ( ledNum == LED_NUM_LEDS ) // that's the buzzer
+				TIM_Cmd ( TIM12, DISABLE );
+			else
+				STM_EVAL_LEDOff ( ledNum );
+			led_state[ledNum] = 0;
+		};
+	};
+};
 
 void LedInit ( )
 {
+	LedGPIOInit ( );
+	LedTimerInit ( );
+
 	STM_EVAL_LEDInit ( 0 );
 	STM_EVAL_LEDInit ( 1 );
 	STM_EVAL_LEDInit ( 2 );
@@ -28,39 +129,57 @@ void LedTimerCallback ( long lTimerID )
 	static uint8_t PulseCounter = 0;
 	uint8_t i;
 
-	for ( i = 0; i < LED_NUM_LEDS; i++ ) if ( led_state[i] == LED_STATE_OFF ) STM_EVAL_LEDOff ( i );
-	for ( i = 0; i < LED_NUM_LEDS; i++ ) if ( led_state[i] == LED_STATE_ON ) STM_EVAL_LEDOn ( i );
-	if ( buzzer_state == LED_STATE_OFF ) ;
-	else if ( buzzer_state == LED_STATE_ON ) ;
-
 	switch ( lTimerID )
 	{
 	case 0:
-		if ( ( PulseCounter % 8 ) == 0 )
+		// If requested state is OFF just switch off the led. This is checked in the short timer callback
+		for ( i = 0; i < LED_NUM_LEDS + 1; i++ ) if ( led_mode[i] == LED_MODE_OFF ) LedOff ( i );
+
+		// If requested state is ON just switch on the led. This is checked in the short timer callback
+		for ( i = 0; i < LED_NUM_LEDS + 1; i++ ) if ( led_mode[i] == LED_MODE_ON ) LedOff ( i );
+
+		if ( ( PulseCounter % LED_FLASH_PERIOD ) == 0 )
 		{
-			for ( i = 0; i < LED_NUM_LEDS; i++ )
+			// switch led on on pulse 0
+			for ( i = 0; i < LED_NUM_LEDS + 1; i++ )
 			{
-				if ( led_state[i] == LED_STATE_FLASH ) STM_EVAL_LEDOn ( i );
-			}
-			if ( buzzer_state == LED_STATE_FLASH ) ;
+				if ( led_mode[i] == LED_MODE_FLASH )
+				{
+					if ( led_pulses[i] == 0 ) led_mode[i] = LED_MODE_OFF;
+					else if ( led_pulses[i] != -1 ) LedOn ( i );
+				};
+			};
 		}
-		else if ( ( PulseCounter % 8 ) == 1 )
+		else if ( ( PulseCounter % LED_FLASH_PERIOD ) == LED_FLASH_DUTY )
 		{
-			for ( i = 0; i < LED_NUM_LEDS; i++ )
+			// switch led off after DUTY has elapsed
+			for ( i = 0; i < LED_NUM_LEDS + 1; i++ )
 			{
-				if ( led_state[i] == LED_STATE_FLASH ) STM_EVAL_LEDOff ( i );
+				if ( led_mode[i] == LED_MODE_FLASH ) STM_EVAL_LEDOff ( i );
 			}
-			if ( buzzer_state == LED_STATE_FLASH ) ;
 		}
 		PulseCounter++;
 		break;
 	case 1:
-		for ( i = 0; i < LED_NUM_LEDS; i++ ) if ( led_state[i] == LED_STATE_BLINK_FAST ) STM_EVAL_LEDToggle ( i );
-		if ( buzzer_state == LED_STATE_BLINK_FAST ) ;
+		for ( i = 0; i < LED_NUM_LEDS + 1; i++ ) if ( led_mode[i] == LED_MODE_BLINK_FAST )
+		{
+			if ( led_pulses[i] == 0 ) led_mode[i] = LED_MODE_OFF;
+			else if ( led_pulses[i] != -1 ) LedToggle ( i );
+		}
 		break;
 	case 2:
-		for ( i = 0; i < LED_NUM_LEDS; i++ ) if ( led_state[i] == LED_STATE_BLINK_SLOW ) STM_EVAL_LEDToggle ( i );
-		if ( buzzer_state == LED_STATE_BLINK_SLOW ) ;
+		for ( i = 0; i < LED_NUM_LEDS + 1; i++ ) if ( led_mode[i] == LED_MODE_BLINK_SLOW )
+		{
+			if ( led_pulses[i] == 0 ) led_mode[i] = LED_MODE_OFF;
+			else if ( led_pulses[i] != -1 ) LedToggle ( i );
+		}
 		break;
 	};
+};
+
+void LedModeSet ( uint8_t ledNum, enum LED_MODE ledMode, uint8_t ledPulses )
+{
+	if ( ledNum >= LED_NUM_LEDS + 1 ) return;
+	led_mode[ledNum] = ledMode;
+	led_pulses[ledNum] = ledPulses;
 };
--- Driver/led_btn_buz/led_btn_buz.h
+++ Driver/led_btn_buz/led_btn_buz.h
@@ -9,21 +9,22 @@
 #define LED_TIMER_PERIOD_SLOW 500
 
 #define LED_NUM_LEDS LEDn
+#define LED_FLASH_PERIOD 8 // this much LED_TIMER_PERIOD_FLASH milliseconds until next flash
+#define LED_FLASH_DUTY 1 // this much LED_TIMER_PERIOD_FLASH milliseconds led on
 
-enum LED_STATE
+enum LED_MODE
 {
-	LED_STATE_OFF,
-	LED_STATE_BLINK_SLOW,
-	LED_STATE_BLINK_FAST,
-	LED_STATE_FLASH,
-	LED_STATE_ON
+	LED_MODE_OFF,			// led is always off
+	LED_MODE_BLINK_SLOW,	// led blinks slow (even on- and off-time)
+	LED_MODE_BLINK_FAST,	// led blinks fast (even on- and off-time)
+	LED_MODE_FLASH,		// short flash, long pause
+	LED_MODE_ON			// led is always on
 };
 
 void LedInit ( );
 void LedTimerCallback ( long ulTimerID );
+void LedModeSet ( uint8_t ledNum, enum LED_MODE ledMode, uint8_t ledPulses );
 
 extern unsigned ledTimerPeriods[LED_NUM_TIMER];
-
-
 
 #endif //  __LED_BTN_BUZ_H__

4ef689d487e5c9c8fe47af435a0017be8a16c11c
Frithjof Siebecke <frithjofsiebecke@web.de>
2014-03-01 13:02:42 +0100

    added config header file for led

---
+++ Driver/led_btn_buz/led_btn_buz_config.h
@@ -0,0 +1,13 @@
+#ifndef __LED_BTN_BUZ_CONFIG_H__
+#define __LED_BTN_BUZ_CONFIG_H__
+
+#define LED_FLASH_PERIOD 8 // this much LED_TIMER_PERIOD_FLASH milliseconds until next flash
+#define LED_FLASH_DUTY 1 // this much LED_TIMER_PERIOD_FLASH milliseconds led on
+
+#define LED_TIMER_PERIOD_FLASH 100
+#define LED_TIMER_PERIOD_FAST 250
+#define LED_TIMER_PERIOD_SLOW 500
+
+// #define LED_ENABLE_BUZZER_PWM
+
+#endif // __LED_BTN_BUZ_CONFIG_H__
